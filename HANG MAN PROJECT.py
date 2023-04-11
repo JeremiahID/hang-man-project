608d661fa105b4310e4317d3829a4464ca6dec02
Idahosa Jeremiah Efe <113017684+JeremiahID@users.noreply.github.com>
2023-04-11 11:35:50 +0100

    Add files via upload

---
+++ HANG MAN PROJECT.py
@@ -0,0 +1,30 @@
+from random import choice
+
+#W_L means word list
+
+W_L=["divine","jeremiah","vanessa","esther","God","samuel","crystal","solution",]
+
+#W means word
+
+W=choice([W_L])
+
+# L means life
+L=10
+
+#L_E means letter entered
+
+L_E=input("ENTER A LETTER: ")
+if L_E in W:
+    print("corect letter")
+elif L_E not in W:
+    print("Letter not found in word ")
+else:
+    print("")
+for L_E in W:
+    while L>0:
+        print("YOU HAVE",L,"TRIALS REMANINIG")
+        L-=1
+        L_E=""
+              
+            
+
